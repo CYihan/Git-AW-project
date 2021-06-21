2aa880a58cdb3d679bb9df1c49063e8f96a38070
Cyihan <yihanc92@live.se>
2021-06-21 14:03:21 +0200

    testable code

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,9 @@ public class Main {
         int a = Integer.MAX_VALUE;
         int b = Integer.MAX_VALUE;
 
-
-        System.out.println(add(a, b));
+        //System.out.println(add(a, b));
         System.out.println(subtract(b, a));
-        System.out.println(multiply(a, b));
-
+        //System.out.println(multiply(a, b));
     }
     private static int subtract(int b, int a) {
         return b - a;
